54c1c6db0e58c95239fc416d705e2835d591fcc6
Chaewoon Kang <kchawoon@naver.com>
2026-03-15 10:36:12 +0900

    feat: add vpc

---
+++ environments/dev/main.tf
@@ -0,0 +1,41 @@
+// vpc
+module "vpc" {
+  source       = "../../modules/vpc"
+  cluster_name = var.cluster_name
+  cidr_block   = "10.1.0.0/16"
+  azs = {
+    a = "ap-northeast-2a"
+    c = "ap-northeast-2c"
+  }
+  public_subnets = {
+    a = {
+      cidr = "10.1.0.0/24"
+      az   = "a"
+    }
+    c = {
+      cidr = "10.1.1.0/24"
+      az   = "c"
+    }
+  }
+  private_subnets = {
+    a = {
+      cidr = "10.1.100.0/24"
+      az   = "a"
+    }
+    c = {
+      cidr = "10.1.101.0/24"
+      az   = "c"
+    }
+  }
+  db_subnets = {
+    a = {
+      cidr = "10.1.200.0/24"
+      az   = "a"
+    }
+    c = {
+      cidr = "10.1.201.0/24"
+      az   = "c"
+    }
+  }
+  natgw_az_keys = ["a", "c"]
+}
