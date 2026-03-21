807a831788bbd43eadc60cbb1d4786acbb9cf385
Chaewoon Kang <kchawoon@naver.com>
2026-03-22 00:34:52 +0900

    fix: deleted egress-all sg

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -65,14 +65,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_vpc" {
   from_port   = 443
   to_port     = 443
 }
-resource "aws_vpc_security_group_egress_rule" "allow_egress_all" {
-  security_group_id = aws_security_group.vpce.id
 
-  cidr_ipv4   = "0.0.0.0/0"
-  ip_protocol = "-1"
-  from_port   = 0
-  to_port     = 0
-}
 module "vpc-endpoints" {
   source = "../../modules/vpc-endpoint"
   vpc_id = module.vpc.vpc_id
