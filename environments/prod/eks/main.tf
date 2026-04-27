f995de6ec164a453efb74325febd7e09a575aeea
Chaewoon Kang <kchawoon@naver.com>
2026-04-27 16:17:14 +0900

    feat: add cert-manager

--- environments/prod/eks/main.tf
+++ environments/prod/eks/main.tf
@@ -363,3 +363,45 @@ resource "kubectl_manifest" "gateway_class" {
     kubectl_manifest.gateway_api
   ]
 }
+
+#######################
+# Helm - Cert Manager #
+#######################
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "oci://quay.io/jetstack/charts"
+  chart            = "cert-manager"
+  version          = "1.20.2"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  values = [
+    yamlencode({
+      crds = {
+        enabled = true
+      }
+      image = {
+        repository = "647502392199.dkr.ecr.ap-northeast-2.amazonaws.com/quay/jetstack/cert-manager-controller"
+      }
+      webhook = {
+        image = {
+          repository = "647502392199.dkr.ecr.ap-northeast-2.amazonaws.com/quay/jetstack/cert-manager-webhook"
+        }
+      }
+      cainjector = {
+        image = {
+          repository = "647502392199.dkr.ecr.ap-northeast-2.amazonaws.com/quay/jetstack/cert-manager-cainjector"
+        }
+      }
+      acmesolver = {
+        image = {
+          repository = "647502392199.dkr.ecr.ap-northeast-2.amazonaws.com/quay/jetstack/cert-manager-acmesolver"
+        }
+      }
+      startupapicheck = {
+        image = {
+          repository = "647502392199.dkr.ecr.ap-northeast-2.amazonaws.com/quay/jetstack/cert-manager-startupapicheck"
+        }
+      }
+  })]
+}
