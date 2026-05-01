7e4c46e3f8595402944f9261e87ba43e6e6e4810
Chaewoon Kang <kchawoon@naver.com>
2026-05-01 16:53:35 +0900

    fix: add missing sqs vpc endpoint

--- environments/dev/infra/main.tf
+++ environments/dev/infra/main.tf
@@ -241,6 +241,13 @@ module "vpc_endpoints" {
       subnet_ids          = module.vpc.private_subnet_ids
       security_group_ids  = [aws_security_group.vpce.id]
     }
+    sqs = {
+      service_name        = "com.amazonaws.${var.aws_region}.sqs"
+      endpoint_type       = "Interface"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnet_ids
+      security_group_ids  = [aws_security_group.vpce.id]
+    }
   }
 }
 
