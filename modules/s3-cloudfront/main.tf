ac08ce823bf0c463ee6c4d9be45d98a1ff9f8a87
Chaewoon Kang <kchawoon@naver.com>
2026-05-13 13:10:25 +0900

    fix: add minimum tls version explicitly

--- modules/s3-cloudfront/main.tf
+++ modules/s3-cloudfront/main.tf
@@ -78,8 +78,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     for_each = var.enable_custom_domain ? [1] : []
 
     content {
-      acm_certificate_arn = var.acm_certificate_arn
-      ssl_support_method  = "sni-only"
+      acm_certificate_arn      = var.acm_certificate_arn
+      ssl_support_method       = "sni-only"
+      minimum_protocol_version = "TLSv1.3_2025"
     }
   }
 
